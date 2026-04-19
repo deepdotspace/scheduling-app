be3c8025d5b497cb2727a20f0ed284a010808576
evch1204 <evanchang1204@gmail.com>
2026-04-19 14:07:27 -0700

    test

--- src/book-me-pages/AssistantPage.tsx
+++ src/book-me-pages/AssistantPage.tsx
@@ -46,9 +46,9 @@ import { UserAccountMenu } from '../components/UserAccountMenu'
 
 function getGreeting(): string {
   const hour = new Date().getHours()
-  if (hour < 12) return 'Good Morning'
-  if (hour < 17) return 'Good Afternoon'
-  return 'Good Evening'
+  if (hour < 12) return 'Good Morning hi'
+  if (hour < 17) return 'Good Afternoon hi'
+  return 'Good Evening hi'
 }
 
 interface AssistantEventData {
