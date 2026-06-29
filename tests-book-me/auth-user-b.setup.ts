3940a613e662667a3f70fcc65c1167770717621a
evan chang <evanchang1204@gmail.com>
2026-06-29 09:27:35 -0700

    update
    - clean up

--- tests-book-me/auth-user-b.setup.ts
+++
@@ -1,2 +0,0 @@
-import { createAuthSetup } from '../../../test-utils/auth-setup'
-createAuthSetup('ctc2@gmail.com', 'at anthropic', 'tests/.auth/session-user-b.json', 'authenticate user B')
--- tests-book-me/auth.setup.ts
+++
@@ -1,2 +0,0 @@
-import { createAuthSetup } from '../../../test-utils/auth-setup'
-createAuthSetup('ctc@gmail.com', 'at anthropic', 'tests/.auth/session.json')
